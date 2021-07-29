b1f1a8c1b45fa72840ce99344a90d107797ab9ed
BrokenShell <webmaster@sharpdesigndigital.com>
2021-07-29 07:06:03 -0700

    visualization update for daily change

--- app/vis.py
+++ app/vis.py
@@ -2,8 +2,9 @@ import pandas as pd
 from fastapi import APIRouter
 import plotly.graph_objects as go
 import plotly.express as px
+import datetime as dt
 
-from app.db import get_club_activity_df_by_date_range
+from app.db import get_club_activity_df_by_date_range, get_df
 
 router = APIRouter()
 
@@ -23,8 +24,6 @@ def get_pie_detail(df: pd.DataFrame, col: str) -> go.Figure:
         colorway=px.colors.qualitative.Antique,
         height=640,
         width=820,
-        paper_bgcolor="rgba(0,0,0,0)",
-        plot_bgcolor="rgba(0,0,0,0)",
     )
     figure = go.Figure(data, layout)
     figure.update_traces(
@@ -49,3 +48,54 @@ def get_pie(club: str, activity: str, start: str, stop: str):
         col='emoji',
     )
     return figure.to_json()
+
+
+def get_bar_daily():
+    clubs = {
+        'Anderson',
+        'Catlin',
+        'Grossman',
+        'Jefferson',
+        'Johnston',
+        'Morton',
+        'Marley',
+        'Notter',
+        'Stelle',
+    }
+    df = get_df()
+    stop = dt.datetime.today()
+    start = stop - dt.timedelta(days=1)
+    df = df[(df['date'] >= start) & (df['date'] <= stop)]
+    club_results = {}
+    for club in clubs:
+        start = df[
+            (df['activity'] == 'Club Checkin') &
+            (df['club'] == club)
+            ].mean(numeric_only=True)[0]
+        stop = df[
+            (df['activity'] == 'Club Checkout') &
+            (df['club'] == club)
+            ].mean(numeric_only=True)[0]
+        average = stop - start
+        club_results[club] = average
+
+    result_df = pd.DataFrame(
+        club_results.values(),
+        index=club_results.keys(),
+        columns=["Daily Net Change"],
+    )
+    return result_df
+
+
+@router.get("/daily/")
+def get_daily():
+    df = get_bar_daily()
+    data = go.Bar(x=df.index, y=df["Daily Net Change"])
+    layout = go.Layout(
+        title="Sentiment: Daily Net Change",
+        height=600,
+        width=820,
+        yaxis={"title": "Checkin/Checkout Difference"},
+        xaxis={'title': "B&G Clubs"},
+    )
+    return go.Figure(data, layout).to_json()
